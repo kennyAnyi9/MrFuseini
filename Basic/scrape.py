750bfddf674a04b63698214c8d16177aa85a1991
kennyAnyi9 <kennyanyi@gmail.com>
2024-03-02 00:27:09 +0000

    add comments to explain stuff better

--- Basic/scrape.py
+++ Basic/scrape.py
@@ -1,18 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
-
-
 # Send request and parse HTML
 #this will print the status code 
 # html_text = requests.get('https://books.toscrape.com/')
 # print(html_text)
 
+#This would print the html
 html_text = requests.get('https://books.toscrape.com/').text
 #print(html_text)
 
+#parse html to BeautifulSoup object with lxml parser
 soup = BeautifulSoup(html_text, 'lxml')
 # print(soup)
 
@@ -30,8 +28,12 @@ soup = BeautifulSoup(html_text, 'lxml')
 books_container = soup.find('section')
 # print(books_container)
 book_list = books_container.find('ol', class_='row')
+#the findall method would find all the tagsin question and put them in a single list
 books = book_list.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
 
+
+#here we iterate over the list in books variable and print the results
+# the interation variable is not really necessary but i just used it to count the number of iterations for the looping to be sure we scrapped all the data.
 iteration =0
 for book in books:
     title = book.h3.text
@@ -46,13 +48,4 @@ print(iteration)
 
 
 
-
-# iteration =0
-# main_container = soup.find('div', class_="hugo4-pc-pagex-root")
-# product_container=main_container.find('div', class_="product-water-fall")
-# product_sub_container=product_container.find('div', class_="hugo4-pc-grid hugo4-pc-grid-5 hugo4-pc-grid-list hugo4-pc-grid-waterfall")
-
-# #trying to scrape all waterfall divs
-# all_waterfall_divs = product_sub_container.find_all('div', class_="waterfall-column")
-# print(all_waterfall_divs)
     
